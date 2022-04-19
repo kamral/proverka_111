bd6c83d0b40b49bab3c24c50fd4466e4190cc987
kamral010101@gmail.com <kamral010101@gmail.com>
2022-04-19 14:09:00 +0200

    we change in polls.models

--- mysites/polls/views.py
+++ mysites/polls/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def detail(request, question_id):
+    return HttpResponse('look up question: ' %question_id)
+
+def results(request, question_id):
+    response='look up question'
+    return HttpResponse(response %question_id)
+
+def vote(request, question_id):
+    return HttpResponse('look up votes: ' %question_id)
